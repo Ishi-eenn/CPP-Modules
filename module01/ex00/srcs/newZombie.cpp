f7e7be7a829e1faf56a1673d10d09756dbe0d9f0
Ishi <syi378039@gmail.com>
2024-02-12 15:08:04 +0900

    fin : module01/ex00

---
+++ module01/ex00/srcs/newZombie.cpp
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   newZombie.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsishika <tsishika@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/11 20:46:14 by tsishika          #+#    #+#             */
+/*   Updated: 2024/02/12 15:05:34 by tsishika         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+Zombie *newZombie(std::string name){
+	Zombie *zombie = new Zombie(name);
+	return zombie;
+}
