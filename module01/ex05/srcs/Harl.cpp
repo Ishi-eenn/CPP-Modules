0e55bcb535c01530f9b77b7c482b3ed45af318ce
Ishi <syi378039@gmail.com>
2024-02-13 11:03:15 +0900

    普通に[INFO level] みたいにした。

--- module01/ex05/srcs/Harl.cpp
+++ module01/ex05/srcs/Harl.cpp
@@ -6,33 +6,33 @@
 /*   By: tsishika <tsishika@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 00:22:43 by tsishika          #+#    #+#             */
-/*   Updated: 2024/02/13 10:57:28 by tsishika         ###   ########.fr       */
+/*   Updated: 2024/02/13 11:02:25 by tsishika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
 void Harl::debug(){
-	std::cout << ":::DEBUG level:::" << std::endl;
+	std::cout << "[DEBUG level]" << std::endl;
 	std::cout << "   I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger." << std::endl;
 	std::cout << "   I really do!" << std::endl;
 }
 
 void Harl::info(){
-	std::cout << ":::INFO level:::" << std::endl;
+	std::cout << "[INFO level]" << std::endl;
 	std::cout << "   I cannot believe adding extra bacon costs more money." << std::endl;
 	std::cout << "   You didn't put enough bacon in my burger!" << std::endl;
 	std::cout << "   If you did, I wouldn't be asking for more!" << std::endl;
 }
 
 void Harl::warning(){
-	std::cout << ":::WARNING level:::" << std::endl;
+	std::cout << "[WARNING level]" << std::endl;
 	std::cout << "   I think I deserve to have some extra bacon for free." << std::endl;
 	std::cout << "   I've been coming for years whereas you started working here since last month." << std::endl;
 }
 
 void Harl::error(){
-	std::cout << ":::ERROR level:::" << std::endl;
+	std::cout << "[ERROR level]" << std::endl;
 	std::cout << "   This is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
@@ -46,5 +46,5 @@ void Harl::complain( std::string level){
 			return;
 		}
 	}
-	std::cout << ":::Unknown level:::" << std::endl;
+	std::cout << "[Unknown level]" << std::endl;
 }
