f7d479e5be39f2f6dbf32643304189dd73e6cb9f
Ishi <syi378039@gmail.com>
2024-04-28 10:14:19 +0900

    fin : ex02

--- ex02/srcs/Fixed.cpp
+++ ex02/srcs/Fixed.cpp
@@ -6,7 +6,174 @@
 /*   By: tsishika <tsishika@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 12:39:53 by tsishika          #+#    #+#             */
-/*   Updated: 2024/04/27 11:39:42 by tsishika         ###   ########.fr       */
+/*   Updated: 2024/04/28 10:11:18 by tsishika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
+
+Fixed::Fixed() : fixedPointValue(0)
+{
+  std::cerr << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int num)
+{
+  std::cerr << "Int constructor called" << std::endl;
+  this->fixedPointValue = num << this->fractionalBits;
+}
+
+Fixed::Fixed(const float num)
+{
+  std::cerr << "Float constructor called" << std::endl;
+  this->fixedPointValue = roundf(num * (1 << this->fractionalBits));
+}
+
+Fixed::Fixed(const Fixed &src)
+{
+  std::cerr << "Copy constructor called" << std::endl;
+  *this = src;
+}
+
+Fixed &Fixed::operator=(const Fixed &src)
+{
+  std::cerr << "Copy assignment operator called" << std::endl;
+  if (this != &src)
+    this->fixedPointValue = src.getRawBits();
+  return (*this);
+}
+
+Fixed::~Fixed()
+{
+  std::cerr << "Destructor called" << std::endl;
+}
+
+bool Fixed::operator>(const Fixed &src) const
+{
+  return (this->getRawBits() > src.getRawBits());
+}
+
+bool Fixed::operator<(const Fixed &src) const
+{
+  return (this->getRawBits() < src.getRawBits());
+}
+
+bool Fixed::operator>=(const Fixed &src) const
+{
+  return (this->getRawBits() >= src.getRawBits());
+}
+
+bool Fixed::operator<=(const Fixed &src) const
+{
+  return (this->getRawBits() <= src.getRawBits());
+}
+
+bool Fixed::operator==(const Fixed &src) const
+{
+  return (this->getRawBits() == src.getRawBits());
+}
+
+bool Fixed::operator!=(const Fixed &src) const
+{
+  return (this->getRawBits() != src.getRawBits());
+}
+
+Fixed Fixed::operator+(const Fixed &src) const
+{
+	return (Fixed(this->toFloat() + src.toFloat()));
+}
+
+Fixed Fixed::operator-(const Fixed &src) const
+{
+	return (Fixed(this->toFloat() - src.toFloat()));
+}
+
+Fixed Fixed::operator*(const Fixed &src) const
+{
+	return (Fixed(this->toFloat() * src.toFloat()));
+}
+
+Fixed Fixed::operator/(const Fixed &src) const
+{
+	return (Fixed(this->toFloat() / src.toFloat()));
+}
+
+Fixed &Fixed::operator++(void)
+{
+  setRawBits(this->getRawBits() + 1);
+  return (*this);
+}
+
+Fixed Fixed::operator++(int)
+{
+  Fixed tmp = *this;
+  setRawBits(this->getRawBits() + 1);
+  return (tmp);
+}
+
+Fixed &Fixed::operator--(void)
+{
+  setRawBits(this->getRawBits() - 1);
+  return (*this);
+}
+
+Fixed Fixed::operator--(int)
+{
+  Fixed tmp = *this;
+  setRawBits(this->getRawBits() - 1);
+  return (tmp);
+}
+
+Fixed &Fixed::min(Fixed &left, Fixed &right)
+{
+  if (left.getRawBits() < right.getRawBits())
+    return (left);
+  return (right);
+}
+
+const Fixed &Fixed::min(const Fixed &left, const Fixed &right)
+{
+  if (left.getRawBits() < right.getRawBits())
+    return (left);
+  return (right);
+}
+
+Fixed &Fixed::max(Fixed &left, Fixed &right)
+{
+  if (left.getRawBits() > right.getRawBits())
+    return (left);
+  return (right);
+}
+
+const Fixed &Fixed::max(const Fixed &left, const Fixed &right)
+{
+  if (left.getRawBits() > right.getRawBits())
+    return (left);
+  return (right);
+}
+
+int Fixed::getRawBits(void) const
+{
+  return (this->fixedPointValue);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+  this->fixedPointValue = raw;
+}
+
+float Fixed::toFloat(void) const
+{
+  return ((float)this->fixedPointValue / (1 << this->fractionalBits));
+}
+
+int Fixed::toInt(void) const
+{
+  return (this->fixedPointValue >> this->fractionalBits);
+}
+
+std::ostream &operator<<(std::ostream &out, const Fixed &src)
+{
+  out << src.toFloat();
+  return (out);
+}
--- ex02/srcs/main.cpp
+++ ex02/srcs/main.cpp
@@ -6,12 +6,27 @@
 /*   By: tsishika <tsishika@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 12:39:41 by tsishika          #+#    #+#             */
-/*   Updated: 2024/04/25 18:03:03 by tsishika         ###   ########.fr       */
+/*   Updated: 2024/04/28 10:13:41 by tsishika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include "Fixed.hpp"
+#include <iostream>
 
-int main(void) {
-  
+int main( void ) {
+
+  Fixed a;
+  Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+
+  std::cout << a << std::endl;
+  std::cout << ++a << std::endl;
+  std::cout << a << std::endl;
+  std::cout << a++ << std::endl;
+  std::cout << a << std::endl;
+
+  std::cout << b << std::endl;
+
+  std::cout << Fixed::max( a, b ) << std::endl;
+
+  return 0;
 }
