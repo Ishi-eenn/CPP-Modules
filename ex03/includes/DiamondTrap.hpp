20a6846e810af286f3461009b5b8145257d4b395
Ishi <syi378039@gmail.com>
2024-04-29 01:48:17 +0900

    ex03 : 実装完了mainまだ

---
+++ ex03/includes/DiamondTrap.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   DiamondTrap.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsishika <tsishika@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/29 01:36:41 by tsishika          #+#    #+#             */
+/*   Updated: 2024/04/29 01:47:50 by tsishika         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DIAMONDTRAP_HPP
+# define DIAMONDTRAP_HPP
+
+#include <iostream>
+#include <string>
+#include "ScavTrap.hpp"
+#include "FragTrap.hpp"
+
+class DiamondTrap : public ScavTrap, public FragTrap {
+  private:
+    std::string name;
+  public:
+    DiamondTrap();
+    DiamondTrap(const std::string &name);
+    DiamondTrap(const DiamondTrap &copy);
+    ~DiamondTrap();
+
+    DiamondTrap &operator=(const DiamondTrap &copy);
+
+    void attack(std::string const &target);
+    void whoAmI();
+};
+
+#endif
