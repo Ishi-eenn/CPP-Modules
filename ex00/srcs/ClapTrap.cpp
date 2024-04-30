07d687ae3c2a50f3227aab9dbbfd939a8f6aa689
Ishi <syi378039@gmail.com>
2024-04-30 23:31:58 +0900

    コンストラクタ、デストラクタをcerrに変更

--- ex00/srcs/ClapTrap.cpp
+++ ex00/srcs/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: tsishika <tsishika@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/21 23:39:37 by tsishika          #+#    #+#             */
-/*   Updated: 2024/04/30 22:34:55 by tsishika         ###   ########.fr       */
+/*   Updated: 2024/04/30 23:30:44 by tsishika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,27 @@
 
 ClapTrap::ClapTrap() : name("tsishika"), hitPoints(10), energyPoints(10), attackDamage(0)
 {
-  std::cout << "ClapTrap default constructor called" << std::endl;
+  std::cerr << "ClapTrap default constructor called" << std::endl;
 }
 
 ClapTrap::ClapTrap(const std::string &name) : name(name), hitPoints(10), energyPoints(10), attackDamage(0) {
-  std::cout << "ClapTrap name constructor called" << std::endl;
+  std::cerr << "ClapTrap name constructor called" << std::endl;
 }
 
 ClapTrap::ClapTrap(const ClapTrap &copy)
 {
-  std::cout << "ClapTrap copy constructor called" << std::endl;
+  std::cerr << "ClapTrap copy constructor called" << std::endl;
   *this = copy;
 }
 
 ClapTrap::~ClapTrap()
 {
-  std::cout << "ClapTrap destructor called" << std::endl;
+  std::cerr << "ClapTrap destructor called" << std::endl;
 }
 
 ClapTrap &ClapTrap::operator=(const ClapTrap &copy)
 {
-  std::cout << "ClapTrap assignation operator called" << std::endl;
+  std::cerr << "ClapTrap assignation operator called" << std::endl;
   if (this != &copy) {
     this->name = copy.name;
     this->hitPoints = copy.hitPoints;
